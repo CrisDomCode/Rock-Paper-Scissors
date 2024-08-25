8e207e089a19917cdf51b60ae0a29fcfa18963d2
CrisDomCode <cristina.dominguez.cd@gmail.com>
2024-08-25 18:21:10 +0200

    1 round OK, included keeping Score

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let computerChoice
-let computerScore
+let computerScore = 0
 let humanChoice
-let humanScore
+let humanScore = 0
 
 function generateRandomNumber(max) {
     return Math.floor(Math.random() * max) + 1;
@@ -28,8 +28,58 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     humanChoice = prompt("What do you want : Rock, Paper, Scissors ?", "Rock");
-    humanChoice = humanChoice.toLowerCase();
-    console.log(humanChoice)
+    return humanChoice
 }
 
-getHumanChoice()
+function playRound(humanChoice, computerChoice) {
+    let message = ""
+    humanChoice = humanChoice.toLowerCase();
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "rock") {
+                message = "Draw, " + computerChoice + " equals " + humanChoice
+            } else if(computerChoice === "paper") {
+                message = "You lose, " + computerChoice + " beats " + humanChoice
+                computerScore ++
+            } else if(computerChoice === "scissors") {
+                message = "You win, " + humanChoice + " beats " + computerChoice
+                humanScore ++
+            }
+            break;
+        case "paper":
+            if (computerChoice === "scissors") {
+                message = "You lose, " + computerChoice + " beats " + humanChoice
+                computerScore ++
+            } else if(computerChoice === "paper") {
+                message = "Draw, " + computerChoice + " equals " + humanChoice
+            } else if(computerChoice === "rock") {
+                message = "You win, " + humanChoice + " beats " + computerChoice
+                humanScore ++
+            }
+            break;
+        case "scissors":
+            if (computerChoice === "scissors") {
+                message = "Draw, " + computerChoice + " equals " + humanChoice
+            } else if(computerChoice === "paper") {
+                message = "You win, " + humanChoice + " beats " + computerChoice
+                humanScore ++
+            } else if(computerChoice === "rock") {
+                message = "You lose, " + computerChoice + " beats " + humanChoice
+                computerScore ++
+            }
+            break;
+        default:
+            break;
+    }
+    console.log(humanChoice, computerChoice)
+    console.log(message)
+    console.log("Score : " + humanScore + " - " + computerScore)
+}
+
+
+
+
+computerChoice = getComputerChoice()
+humanChoice = getHumanChoice()
+
+playRound(humanChoice, computerChoice)
